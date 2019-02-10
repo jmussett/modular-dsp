c14efd9300170db7132496024020aa0a46ef5b98
James Mussett <jamesmussett@gmail.com>
2019-02-10 22:51:14 +0000

    Added midi playback support

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate num_derive;
 extern crate num_traits;
 extern crate portaudio;
+extern crate portmidi;
 extern crate crossbeam_channel;
 extern crate audio_thread_priority;
 
@@ -10,7 +11,9 @@ mod audio;
 
 use audio::{AudioModule, Command};
 use sine::{SineModule, SineParameter};
-use portaudio::{error::Error, PortAudio};
+use portaudio::{PortAudio};
+use portmidi::{PortMidi};
+use crossbeam_channel::Sender;
 
 const CHANNELS: usize = 2;
 const SAMPLE_RATE: f32 = 44_100.0;
@@ -19,29 +22,83 @@ const SAMPLES_PER_BUFFER: usize = FRAMES_PER_BUFFER * CHANNELS;
 
 fn main() {
     let (command_sender, command_receiver) = crossbeam_channel::bounded(1024);
-
+    
     std::thread::spawn(move || {
         match play_audio::<SineModule>(command_receiver) {
             Ok(_) => {},
-            e => eprintln!("Error: {:?}", e)
+            Err(e) => eprintln!("Audio Error: {:?}", e)
         }
     });
 
+    let midi_sender = command_sender.clone();
+
+    std::thread::spawn(move || {
+        match process_midi(midi_sender) {
+            Ok(_) => {},
+            Err(e) => eprintln!("Midi Error: {:?}", e)
+        }
+    });
+
+    match process_inputs(command_sender) {
+        Ok(_) => {},
+        Err(e) => eprintln!("Input Error: {:?}", e)
+    }
+}
+
+fn process_midi(command_sender: Sender<Command>) -> Result<(), portmidi::Error> {
+    let pm = PortMidi::new()?;
+
+    let default_device_id = pm.default_input_device_id()?;
+    let device = pm.device(default_device_id)?;
+
+    println!("Default Midi Input Device: {:#?}", device.name());
+
+    let midi_port = pm.input_port(device, 1024)?;
+
+    while let Ok(_) = midi_port.poll() {
+        if let Ok(Some(events)) = midi_port.read_n(1024) {
+           for event in events {
+                match event.message.status {
+                    // Note Off
+                    0x80 => {
+                        command_sender.send(Command::SetParameter(SineParameter::Frequency as usize, 0.0)).unwrap();
+                    },
+                    // Note On
+                    0x90 => {
+                        let note = event.message.data1 as f32;
+                        let frequency = 27.5 * 2f32.powf((note - 21.0)/12.0);
+
+                        println!("Note: {:#?}, Frequency: {:#?}", note, frequency);
+
+                        command_sender.send(Command::SetParameter(SineParameter::Frequency as usize, frequency)).unwrap();
+                    },
+                    _ => println!("Midi Status Not Supported: {:x?}", event.message.status)
+                }
+            }
+        }
+    }
+
+    println!("Midi Input Device Disconnected");
+
+    Ok(())
+}
+
+fn process_inputs(command_sender: Sender<Command>) -> Result<(), std::io::Error> {
     loop {
         let mut input = String::new();
-        match std::io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                let frequency = input.trim().parse::<f32>().unwrap();
-                command_sender.send(Command::SetParameter(SineParameter::Frequency as usize, frequency)).unwrap()
-            },
-            Err(e) => eprintln!("Error: {:?}", e)
+        std::io::stdin().read_line(&mut input)?;
+        match input.trim().parse::<f32>() {
+            Ok(frequency) => {
+                command_sender.send(Command::SetParameter(SineParameter::Frequency as usize, frequency)).unwrap();
+            }
+            Err(_) => eprintln!("{:?} was not a number", input.trim())
         }
     }
 }
 
 fn play_audio<Module: AudioModule>(
     command_receiver: crossbeam_channel::Receiver<Command>
-) -> Result<(), Error> {
+) -> Result<(), portaudio::Error> {
     let pa = PortAudio::new()?;
 
     log_host(&pa)?;
@@ -71,7 +128,7 @@ fn play_audio<Module: AudioModule>(
         }
 
         match stream.read(FRAMES_PER_BUFFER as u32) {
-            Err(Error::InputOverflowed) => println!("Input underflowed"),
+            Err(portaudio::Error::InputOverflowed) => println!("Input underflowed"),
             Err(err) => println!("Read from stream failed - {:?}", err),
             Ok(input) => {
                 assert_eq!(input.len(), SAMPLES_PER_BUFFER);
@@ -83,14 +140,14 @@ fn play_audio<Module: AudioModule>(
             assert_eq!(output.len(), SAMPLES_PER_BUFFER);
             audio_module.process_stereo(&input_buffer[0..SAMPLES_PER_BUFFER], output);
         }) {
-            Err(Error::OutputUnderflowed) => println!("Output underflowed"),
+            Err(portaudio::Error::OutputUnderflowed) => println!("Output underflowed"),
             Err(err) => println!("Write to stream failed - {:?}", err),
             _ => (),
         };
     }
 }
 
-fn log_host(pa: &PortAudio) -> Result<(), Error>  {
+fn log_host(pa: &PortAudio) -> Result<(), portaudio::Error>  {
     let host_index = pa.default_host_api()?;
     let default_host = pa.host_apis().find(|host| host.0 == host_index)
         .expect("Default Host does not exist").1;
@@ -100,7 +157,7 @@ fn log_host(pa: &PortAudio) -> Result<(), Error>  {
     Ok(())
 }
 
-fn log_devices(pa: &PortAudio) -> Result<(), Error> {
+fn log_devices(pa: &PortAudio) -> Result<(), portaudio::Error> {
     let input_device_index = pa.default_input_device()?;
     let output_device_index = pa.default_output_device()?;
 
@@ -110,8 +167,8 @@ fn log_devices(pa: &PortAudio) -> Result<(), Error> {
     let output_device = pa.devices()?.find(|device| device.as_ref().unwrap().0 == output_device_index)
         .expect("Default Input Device does not exist")?.1;
 
-    println!("Default Input Device: {:#?}", input_device.name);
-    println!("Default Output Device: {:#?}", output_device.name);
+    println!("Default Audio Input Device: {:#?}", input_device.name);
+    println!("Default Audio Output Device: {:#?}", output_device.name);
 
     Ok(())
 }
--- src/sine/sinemodule.rs
+++ src/sine/sinemodule.rs
@@ -2,6 +2,8 @@ use audio::{AudioModule, Command};
 use std::f64::consts::PI;
 use num_traits::FromPrimitive;
 
+const LOOKUP_SIZE: usize = 1000;
+
 #[derive(FromPrimitive)]
 pub enum SineParameter {
     Frequency
@@ -10,50 +12,54 @@ pub enum SineParameter {
 pub struct SineModule {
     left_phase: usize,
     right_phase: usize,
-    sine: Vec<f32>,
+    frequency: f32,
+    lookup_table: Vec<f32>,
     sample_rate: f32
-}
-
-impl SineModule {
-    fn calculate_sine(frequency: f32, sample_rate: f32) -> Vec<f32> {
-        let lookup_size = (sample_rate / frequency).round() as usize;
-        let sine = &mut vec![0.0; lookup_size];
-
-        for i in 0..lookup_size {
-            sine[i] = (i as f64 / lookup_size as f64 * PI * 2.0).sin() as f32;
-        }
-
-        sine.to_vec()
-    }
 }
 
 impl AudioModule for SineModule {
     fn new(sample_rate: f32) -> SineModule {
+        let lookup_table = &mut vec![0.0; LOOKUP_SIZE];
+
+        for i in 0..LOOKUP_SIZE {
+            lookup_table[i] = (i as f64 / LOOKUP_SIZE as f64 * PI * 2.0).sin() as f32;
+        }
+
         SineModule {
-            sine: SineModule::calculate_sine(500.0, sample_rate),
+            lookup_table: lookup_table.to_vec(),
+            frequency: 0.0,
             left_phase: 0,
             right_phase: 0,
             sample_rate: sample_rate
         }
     }
     fn process_stereo(&mut self, input: &[f32], output: &mut [f32]) {
-        let lookup_size = self.sine.len();
+        if self.frequency == 0.0 {
+            for i in 0..input.len()
+            {
+                output[i] = 0.0;
+            }
+            return
+        }
+        
+        let lookup_size = self.lookup_table.len();
+
+        let step = (LOOKUP_SIZE as f32 / (self.sample_rate / self.frequency)) as usize;
+
         for i in (0..input.len()).into_iter().step_by(2) {
-            output[i]   = self.sine[self.left_phase];
-            output[i+1] = self.sine[self.right_phase];
-            self.left_phase += 1;
+            output[i]   = self.lookup_table[self.left_phase];
+            output[i+1] = self.lookup_table[self.right_phase];
+            self.left_phase += step;
             if self.left_phase >= lookup_size { self.left_phase -= lookup_size; }
-            self.right_phase += 1;
+            self.right_phase += step;
             if self.right_phase >= lookup_size { self.right_phase -= lookup_size }
         }
     }
     fn handle_command(&mut self, command: Command) {
         match command {
-            Command::SetParameter(id, value) => match SineParameter::from_usize(id).unwrap() {
+            Command::SetParameter(id, frequency) => match SineParameter::from_usize(id).unwrap() {
                 SineParameter::Frequency => {
-                    self.sine = SineModule::calculate_sine(value, self.sample_rate);
-                    self.left_phase = 0;
-                    self.right_phase = 0;
+                    self.frequency = frequency;
                 }
             },
         }
