4a38c11596a61c50ae2552207da139a1db59b3c1
James Mussett <jamesmussett@gmail.com>
2019-02-10 09:10:09 +0000

    Added support for commands

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,110 @@
+#[macro_use]
+extern crate num_derive;
+extern crate num_traits;
 extern crate portaudio;
+extern crate crossbeam_channel;
+extern crate audio_thread_priority;
 
 use portaudio::{error::Error, PortAudio};
 use std::f64::consts::PI;
+use num_traits::FromPrimitive;
 
 const CHANNELS: usize = 2;
 const SAMPLE_RATE: f32 = 44_100.0;
 const FRAMES_PER_BUFFER: usize = 128;
 const SAMPLES_PER_BUFFER: usize = FRAMES_PER_BUFFER * CHANNELS;
-const SINE_FREQUENCY: f32 = 1000.0;
+
+enum Command {
+    SetParameter(usize, f32)
+}
 
 trait AudioModule {
     fn new() -> Self;
     fn process_stereo(&mut self, input: &[f32], output: &mut [f32]);
+    fn handle_command(&mut self, command: Command);
 }
 
-pub struct SineModule {
+#[derive(FromPrimitive)]
+enum SineParameter {
+    Frequency
+}
+
+struct SineModule {
     left_phase: usize,
     right_phase: usize,
-    sine: Vec<f32>,
-    lookup_size: usize
+    sine: Vec<f32>
 }
 
-impl AudioModule for SineModule {
-    fn new() -> SineModule {
-        let lookup_size = (SAMPLE_RATE / SINE_FREQUENCY).round() as usize;
+impl SineModule {
+    fn calculate_sine(frequency: f32) -> Vec<f32> {
+        let lookup_size = (SAMPLE_RATE / frequency).round() as usize;
         let sine = &mut vec![0.0; lookup_size];
 
         for i in 0..lookup_size {
             sine[i] = (i as f64 / lookup_size as f64 * PI * 2.0).sin() as f32;
         }
 
+        sine.to_vec()
+    }
+}
+
+impl AudioModule for SineModule {
+    fn new() -> SineModule {
         SineModule {
-            sine: sine.to_vec(),
-            lookup_size: lookup_size,
+            sine: SineModule::calculate_sine(500.0),
             left_phase: 0,
             right_phase: 0
         }
     }
     fn process_stereo(&mut self, input: &[f32], output: &mut [f32]) {
+        let lookup_size = self.sine.len();
         for i in (0..input.len()).into_iter().step_by(2) {
             output[i]   = self.sine[self.left_phase];
             output[i+1] = self.sine[self.right_phase];
             self.left_phase += 1;
-            if self.left_phase >= self.lookup_size { self.left_phase -= self.lookup_size; }
+            if self.left_phase >= lookup_size { self.left_phase -= lookup_size; }
             self.right_phase += 1;
-            if self.right_phase >= self.lookup_size { self.right_phase -= self.lookup_size; }
+            if self.right_phase >= lookup_size { self.right_phase -= lookup_size }
+        }
+    }
+    fn handle_command(&mut self, command: Command) {
+        match command {
+            Command::SetParameter(id, value) => match SineParameter::from_usize(id).unwrap() {
+                SineParameter::Frequency => {
+                    self.sine = SineModule::calculate_sine(value);
+                    self.left_phase = 0;
+                    self.right_phase = 0;
+                }
+            },
         }
     }
 }
 
 fn main() {
-    match play_audio::<SineModule>() {
-        Ok(_) => {},
-        e => eprintln!("Example failed with the following: {:?}", e)
+    let (command_sender, command_receiver) = crossbeam_channel::bounded(1024);
+
+    std::thread::spawn(move || {
+        match play_audio::<SineModule>(command_receiver) {
+            Ok(_) => {},
+            e => eprintln!("Example failed with the following: {:?}", e)
+        }
+    });
+
+    loop {
+        let mut input = String::new();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let frequency = input.trim().parse::<f32>().unwrap();
+                command_sender.send(Command::SetParameter(SineParameter::Frequency as usize, frequency)).unwrap()
+            },
+            Err(_) => panic!("Wubalubadubdubdub")
+        }
     }
 }
 
-fn play_audio<Module: AudioModule>() -> Result<(), Error> {
+fn play_audio<Module: AudioModule>(
+    command_receiver: crossbeam_channel::Receiver<Command>
+) -> Result<(), Error> {
     let pa = PortAudio::new()?;
 
     log_host(&pa)?;
@@ -75,7 +123,16 @@ fn play_audio<Module: AudioModule>() -> Result<(), Error> {
     let mut input_buffer = [0.0f32; SAMPLES_PER_BUFFER];
     let audio_module = &mut Module::new();
 
+    audio_thread_priority::promote_current_thread_to_real_time(
+        SAMPLES_PER_BUFFER as u32,
+        SAMPLE_RATE as u32,
+    ).unwrap();
+
     loop {
+        while let Ok(command) = command_receiver.try_recv() {
+            audio_module.handle_command(command);
+        }
+
         match stream.read(FRAMES_PER_BUFFER as u32) {
             Err(Error::InputOverflowed) => println!("Input underflowed"),
             Err(err) => println!("Read from stream failed - {:?}", err),
